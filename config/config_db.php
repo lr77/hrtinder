6aa4f54eaa93c819f035ffea52242384d49502c4
lr77 <83038014+lr77@users.noreply.github.com>
2022-01-21 21:55:23 +0300

    Delete config directory

--- config/config_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'dsn' => 'mysql:host=localhost;dbname=hr_db;charset=utf8',
-    'user' => 'root',
-    'pass' => '',
-];
--- config/params.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'admin_email' => 'admin@mail.com',
-    'base_name' => 'HR-Tinder',
-    'pagination' => 3,
-    'smtp_login' => '',
-    'smtp_password' => '',
-];
